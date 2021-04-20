b28d465702bc3f13051a1411d449628f584d91da
Paulo Vasques <paulovasques88@gmail.com>
2021-04-19 23:16:37 -0300

    Firt Commit

---
+++ src/main/java/one/digitalinnovation/beerstock/enums/BeerType.java
@@ -0,0 +1,23 @@
+package one.digitalinnovation.beerstock.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum BeerType {
+
+    LAGER("Lager"),
+    MALZBIER("Malzbier"),
+    WITBIER("Witbier"),
+    WEISS("Weiss"),
+    ALE("Ale"),
+    IPA("IPA"),
+    STOUT("Stout"),
+    RED("Red"),
+    APA("APA"),
+    SESSION("Sesion");
+
+
+    private final String description;
+}
